c161dc7fac6a522e98aa721779abd4cc6268ad33
James Page <jcpage02@outlook.com>
2019-01-02 11:24:37 -0700

    App CSS wireframed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className='Header'>
-          <branding className='Branding'>
+        <header className="Header">
+          <branding className="Branding">
             <logo>DW</logo>
             <h4>Driftwood</h4>
           </branding>
-          <nav className='TopNav'>
+          <nav className="TopNav">
             <h4>Men's</h4>
             <h4>Women's</h4>
             <h4>Kid's</h4>
             <icon>Cart</icon>
           </nav>
         </header>
-        <image className='Welcome'>Welcome Photo</image>
-        <nav className='SideNav'></nav>
-        <products className='Products'>Products</products>
+        <image className="Welcome">Welcome Photo</image>
+        <nav className="SideNav">Side Nav</nav>
+        <products className="Products">Products</products>
       </div>
     );
   }
